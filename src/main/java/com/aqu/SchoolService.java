a88e1cd6e9a7fbe204a60b8d123ae39784a87a32
aqueel <aqueelansari1@gmail.com>
2017-12-15 08:12:38 +0530

    Initial checkin

---
+++ src/main/java/com/aqu/SchoolService.java
@@ -0,0 +1,23 @@
+package com.aqu;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value="/api/school")
+class SchoolService {
+
+    @RequestMapping(value="/students/{id}")
+    public Student getStudent(@PathVariable String id){
+
+        String URL="http://localhost:8080/api/student/"+id;
+
+        RestTemplate template=new RestTemplate();
+
+        Student student =template.getForObject(URL,Student.class);
+
+        return student;
+    }
+}
